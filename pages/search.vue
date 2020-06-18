ac9a4fe719d992b29d8b6295f412896a02f6427e
zane <ethan.zane.chiang@gmail.com>
2020-06-18 13:26:32 -0400

    added Mete's card design, added chevron icon but doesn't properly display more highlights

--- pages/search.vue
+++ pages/search.vue
@@ -1,38 +1,56 @@
 <template>
   <v-row align="start" justify="start" no-gutters>
-    <v-col v-for="(doc, i) in hits" :key="i + '-' + doc._id" cols="12" md="4">
-      <v-card color="primary" hover>
-        <v-list two-line>
-          <v-list-item-group v-model="selected" active-class="pink--text">
-            <template v-for="(item, index) in items">
-              <v-list-item :key="item.title">
-                <template v-slot:default="{ active }">
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.title">
-                      {{ doc._source.meta.title }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle class="text--primary">
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-                  <v-list-item-action>
-                    <v-list-item-action-text v-text="item.action">
-                    </v-list-item-action-text>
-                    <v-icon v-if="!active" color="grey lighten-1">
-                      star_border
-                    </v-icon>
-                    <v-icon v-else color="yellow">
-                      star
-                    </v-icon>
-                  </v-list-item-action>
-                </template>
-              </v-list-item>
-              <v-divider
-                v-if="index + 1 < items.length"
-                :key="index"
-              ></v-divider>
-            </template>
-          </v-list-item-group>
-        </v-list>
+    <!-- Used md="9" for 9-column width of the v-col -->
+    <v-col v-for="(doc, i) in hits" :key="i + '-' + doc._id" cols="12" md="9">
+      <v-card class="mx-md-10 my-md-1">
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <div>
+            <!-- Used pb-md-0 for removing margins from bottom in md screen -->
+            <v-card-subtitle class="pb-md-0">
+              {{ doc._source.file.filename }}
+            </v-card-subtitle>
+            <!-- Used pt-md-0 for removing margins from top in md screen -->
+            <!-- Used color: blue for blue color -->
+            <v-card-title style="color: blue;" class="pt-md-0">
+              {{ doc._source.meta.title }}
+            </v-card-title>
+            <!-- Used pb-md-0 for removing margins from bottom in md screen -->
+            <v-card-subtitle class="pb-md-0">
+              {{ doc._source.file.url }}
+            </v-card-subtitle>
+            <v-card-subtitle>
+              <ul>
+                <li
+                  v-for="phrase in doc.highlight.content"
+                  :key="phrase"
+                  v-html="phrase"
+                ></li>
+              </ul>
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{
+                  show ? 'mdi-chevron-up' : 'mdi-chevron-down'
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+                <v-card-subtitle>
+                  <ul>
+                    <li
+                      v-for="phrase in doc.highlight.content"
+                      :key="phrase"
+                      v-html="phrase"
+                    ></li>
+                  </ul>
+                </v-card-subtitle>
+              </div>
+            </v-expand-transition>
+          </div>
+        </div>
       </v-card>
     </v-col>
   </v-row>
@@ -62,7 +80,7 @@ export default {
           pre_tags: ['<mark>'],
           post_tags: ['</mark>'],
           fields: {
-            content: { number_of_fragments: 10 }
+            content: { number_of_fragments: 5 }
           }
         }
       })
