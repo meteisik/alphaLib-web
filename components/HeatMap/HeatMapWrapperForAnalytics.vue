3461e3ca756f82272d9c4395a711e17d2230c365
Amir Haghighati <haghighati.amir@gmail.com>
2020-06-26 05:46:36 -0400

    analytics enhancement: add heatmap for queries

---
+++ components/HeatMap/HeatMapWrapperForAnalytics.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-card
+    class="mx-auto"
+    :outlined="outlined"
+    :flat="flat"
+    :hover="hover"
+    :rounded="rounded"
+    :max-width="maxWidth"
+  >
+    <v-card-title>
+      {{ label }}
+    </v-card-title>
+    <v-card-text :id="divId">
+      <heat-map
+        :svg-id="svgId"
+        :width="chartWidth"
+        :height="chartHeight"
+        :color-range="colorRange"
+        :dataset="heatmapData"
+        :padding="{ top: 25, right: 2, left: 300, bottom: 2 }"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import HeatMap from './TheHeatMap'
+export default {
+  name: 'HeatMapWrapperForAnalytics',
+  components: {
+    'heat-map': HeatMap
+  },
+  props: {
+    id: {
+      type: String,
+      default: 'heatmap'
+    },
+    divId: {
+      type: String,
+      default: 'heatmap-div'
+    },
+    svgId: {
+      type: String,
+      default: 'heatmap-svg-element'
+    },
+    chartHeight: {
+      type: Number,
+      default: 100
+    },
+    chartWidth: {
+      type: Number,
+      default: 100
+    },
+    flat: {
+      type: Boolean,
+      default: true
+    },
+    outlined: {
+      type: Boolean,
+      default: true
+    },
+    hover: {
+      type: Boolean,
+      default: true
+    },
+    rounded: {
+      type: Boolean,
+      default: true
+    },
+    maxWidth: {
+      type: String,
+      default: '100%'
+    },
+    label: {
+      type: String,
+      default: 'This Amazing HeatMap'
+    },
+    dataset: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  asyncData() {
+    return {}
+  },
+  data() {
+    return {
+      colorRange: ['#b7e794', '#07bb0d'],
+      meta: {
+        show: false,
+        info: 'Hello this is only a help box!'
+      }
+    }
+  },
+  computed: {
+    heatmapData() {
+      const res = []
+      for (const transaction of this.dataset) {
+        for (const doc of transaction.response.hits.hits) {
+          console.log(doc._score)
+          res.push({
+            x: transaction.query,
+            y: doc._source.path.virtual,
+            value: doc._score
+          })
+        }
+      }
+      return res
+    }
+  }
+}
+</script>
--- pages/analytics/index.vue
+++ pages/analytics/index.vue
@@ -1,30 +1,117 @@
 <template>
-  <div>
-    <div>
-      <input v-model="idInput" type="text" />
-      <button @click="onIdInput">CLICK HERE TO SEND REQUEST</button>
-    </div>
-    <p>{{ idInput }}</p>
-    <nuxt-child />
-  </div>
+  <v-row align="center" justify="start" no-gutters>
+    <v-col cols="1" align-self="center">
+      Queries...
+      <ul>
+        <li v-for="(q, i) in queries" :key="i">
+          {{ q.query }}
+        </li>
+      </ul>
+      <v-btn @click.stop="$fetch">Refresh</v-btn>
+    </v-col>
+    <v-col cols="11">
+      <HeatMapWrapperForAnalytics
+        :id="charts.heatmap.id"
+        class="mb-2"
+        :div-id="charts.heatmap.divId"
+        :svg-id="charts.heatmap.svgId"
+        :hover="false"
+        :label="charts.heatmap.label"
+        :dataset="responses"
+        :chart-width="charts.heatmap.width"
+        :chart-height="charts.heatmap.height"
+        max-width="100%"
+      ></HeatMapWrapperForAnalytics>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import HeatMapWrapperForAnalytics from '~/components/HeatMap/HeatMapWrapperForAnalytics'
 export default {
   name: 'PageAnalyticsIndex',
+  components: { HeatMapWrapperForAnalytics },
+  layout: 'withsearchbar',
+  async fetch() {
+    const res = []
+    for (const q of this.queries) {
+      const tmp = await this.$axios
+        .get('/api/literature/_search', {
+          explain: true,
+          sort: ['_score'],
+          _source: ['meta', 'path'],
+          size: 1,
+          query: {
+            match: {
+              content: {
+                query: q.query
+              }
+            }
+          },
+          highlight: {
+            type: 'unified',
+            order: 'score',
+            pre_tags: ['<mark>'],
+            post_tags: ['</mark>'],
+            fields: {
+              content: { number_of_fragments: 1000 }
+            }
+          }
+        })
+        .then((res) => {
+          return res.data
+        })
+        .catch((e) => {
+          return null
+        })
+      res.push({
+        query: q.query,
+        response: tmp
+      })
+    }
+    console.log(res)
+    this.responses = res
+  },
   data() {
     return {
-      idInput: 'ID here'
+      charts: {
+        heatmap: {
+          id: 'heatmap',
+          divId: 'heatmap-div',
+          svgId: 'heatmap-chart-svg-element',
+          label: 'Tiles',
+          width: 300,
+          height: 100
+        }
+      },
+      responses: []
     }
   },
   computed: {
-    id() {
-      return this.$route.params.id
+    queries() {
+      return this.$store.state.searchQueries
+    },
+    heatmapHeight() {
+      return 500
     }
   },
+  mounted() {
+    this.resize()
+    window.addEventListener('resize', this.resize)
+  },
   methods: {
-    onIdInput() {
-      this.$router.push('/analytics/' + this.idInput)
+    resize() {
+      const heatDiv = document.getElementById(this.charts.heatmap.divId)
+      if (heatDiv) {
+        this.charts.heatmap.width = heatDiv.clientWidth - 30
+        this.charts.heatmap.height = this.heatmapHeight
+        document
+          .getElementById(this.charts.heatmap.svgId)
+          .setAttribute('width', this.charts.heatmap.width)
+        document
+          .getElementById(this.charts.heatmap.svgId)
+          .setAttribute('height', this.charts.heatmap.height)
+      }
     }
   }
 }
