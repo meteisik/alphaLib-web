1cd34843200b6a3cde89cac042d38e3d77de327f
Amir Haghighati <haghighati.amir@gmail.com>
2020-07-08 20:59:51 -0400

    add searchbar

---
+++ components/SearchBar.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-autocomplete
+    v-model="selectedSuggestion"
+    :search-input.sync="query"
+    :items="hits"
+    :loading="isLoading"
+    :outlined="!solo"
+    :solo="solo"
+    clearable
+    rounded
+    single-line
+    autofocus
+    full-width
+    prepend-inner-icon="mdi-magnify"
+    color="primary"
+    hide-no-data
+    hide-selected
+    :placeholder="placeholder"
+    item-text="_source.file.filename"
+    item-value="_source.file.filename"
+    return-object
+    @keyup.native.enter="enterPressed"
+  >
+    <tempate v-slot:no-data>
+      <v-list-item-title>
+        {{ 'Not Specified' }}
+      </v-list-item-title>
+    </tempate>
+    <template v-slot:item="{ item }">
+      <v-icon color="grey">fas fa-list</v-icon>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ item._source.meta.title }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ 'By: ' + item._source.meta.url }}
+        </v-list-item-subtitle>
+        <v-list-item-subtitle :key="item._id">
+          <ul>
+            <li
+              v-for="phrase in item.highlight.content"
+              :key="phrase"
+              v-html="phrase"
+            ></li>
+          </ul>
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    placeholder: {
+      type: String,
+      default: 'SearchBar ...'
+    },
+    solo: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      isLoading: false,
+      suggestions: null,
+      query: '',
+      route: '',
+      selectedSuggestion: null
+    }
+  },
+  computed: {
+    hits() {
+      if (!this.suggestions) return []
+      if (this.suggestions.hits)
+        if (this.suggestions.hits.hits) return this.suggestions.hits.hits
+      return []
+    }
+  },
+  watch: {
+    query(val) {
+      this.$emit('typed', val)
+      this.$fetch()
+    },
+    selectedSuggestion(val) {
+      this.$emit('selected', val)
+    }
+  },
+  async fetch() {
+    this.isLoading = true
+    const q = this.query
+    const suggestions = await this.$axios
+      .post('/api/literature/_search', {
+        explain: true,
+        sort: ['_score'],
+        _source: ['meta', 'file', 'path'],
+        query: {
+          multi_match: {
+            query: q,
+            type: 'bool_prefix',
+            fields: ['meta', 'content']
+          }
+        },
+        highlight: {
+          type: 'unified',
+          order: 'score',
+          pre_tags: ['<mark>'],
+          post_tags: ['</mark>'],
+          fields: {
+            content: { number_of_fragments: 3 }
+          }
+        }
+      })
+      .then((res) => {
+        return res.data
+      })
+      .catch((e) => {
+        console.log(e)
+        return {}
+      })
+    this.isLoading = false
+    this.suggestions = suggestions
+  },
+  methods: {
+    enterPressed() {
+      this.$emit('enter', this.query)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,30 +7,11 @@
     <v-col cols="12" align-self="center" class="text-center">
       <v-row align="center" justify="center">
         <v-col cols="12" md="6" align-self="center">
-          <v-autocomplete
-            v-model="query"
-            :search-input.sync="search"
-            :items="hits"
-            :loading="isLoading"
-            :outlined="!solo"
-            :solo="solo"
-            rounded
-            single-line
-            auto-select-first
-            autofocus
-            cache-items
-            full-width
-            prepend-inner-icon="mdi-magnify"
-            color="primary"
-            hide-no-data
-            hide-selected
-            item-text="_source.meta.title"
-            item-value="_source.meta.author"
-            placeholder="The meaning of life..."
-            return-object
-            @keyup.native.enter="doSearch"
-          >
-          </v-autocomplete>
+          <SearchBar
+            @selected="suggestionSelected"
+            @enter="doSearch"
+            @typed="typed"
+          />
           <v-btn link @click.stop="doSearch">Search</v-btn>
         </v-col>
       </v-row>
@@ -67,10 +48,12 @@
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import SearchBar from '~/components/SearchBar'
 
 export default {
   name: 'PageIndex',
   components: {
+    SearchBar,
     Logo,
     VuetifyLogo
   },
@@ -79,76 +62,34 @@ export default {
       isLoading: false,
       solo: false,
       query: '',
-      search: '',
-      suggestions: null
+      selectedSuggestion: null
     }
   },
   computed: {
-    hits() {
-      if (this.suggestions === null) return []
-      if (Object.keys(this.suggestions).includes('hits'))
-        if (Object.keys(this.suggestions.hits).includes('hits'))
-          return this.suggestions.hits.hits
-      return []
-    },
     history() {
       return this.$store.state.searchQueries
     }
   },
   watch: {
-    search(val) {
-      this.querySuggestions(val)
+    query(val) {
+      // this.querySuggestions(val)
     }
   },
   methods: {
-    async lookUp(el) {
-      const q = this.query
-      const res = await this.$axios.post('/api/literature/_search', {
-        explain: true,
-        sort: ['_score'],
-        query: {
-          multi_match: {
-            query: q,
-            type: 'bool_prefix',
-            fields: ['content', 'meta.*']
-          }
-        }
-      })
-      this.search = res.data
+    doSearch(query) {
+      console.log('doing search on: ', this.query)
+      this.typed(query)
+      this.$store.commit('ADD_QUERY', this.query)
+      this.$router.push('/docs?q=' + this.query)
     },
-    async querySuggestions(q) {
-      this.isLoading = true
-      const suggestions = await this.$axios
-        .post('/api/literature/_search', {
-          explain: true,
-          _source: ['path'],
-          query: {
-            multi_match: {
-              query: q,
-              type: 'bool_prefix',
-              fields: ['meta', 'content']
-            }
-          },
-          highlight: {
-            type: 'unified',
-            order: 'score',
-            fields: {
-              content: { number_of_fragments: 200 }
-            }
-          }
-        })
-        .then((res) => {
-          return res.data
-        })
-        .catch((e) => {
-          return {}
-        })
-      this.isLoading = false
-      this.suggestions = suggestions
+    typed(phrase) {
+      console.log('typed: ', phrase)
+      this.query = phrase
     },
-    doSearch() {
-      this.$store.commit('ADD_QUERY', this.search)
-      this.$router.push('/docs?q=' + this.search)
+    suggestionSelected(suggestion) {
+      this.suggestionSelected = suggestion
+      this.$store.commit('ADD_QUERY', suggestion._source.file.filename)
+      this.$router.push('/docs/' + suggestion._id)
     },
     removeQueryFromHistory(searchItem) {
       this.$store.commit('REMOVE_QUERY', searchItem.query)
